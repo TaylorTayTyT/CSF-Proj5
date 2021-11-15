537f26f86f8b7436c3925babf7177a7942a21130
TaylorTayTyT <68358919+TaylorTayTyT@users.noreply.github.com>
2021-11-15 21:21:28 +0000

    Initial commit

---
+++ calcServer.c
@@ -0,0 +1,89 @@
+#include <stdio.h>      /* for snprintf */
+#include "csapp.h"
+#include "calc.h"
+
+int create_server_socket(int port) {
+	struct sockaddr_in serveraddr = {0};
+	int ssock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (ssock_fd < 0)
+	return -1;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+	if (bind(ssock_fd, (struct sockaddr *) &serveraddr,
+	sizeof(serveraddr)) < 0)
+	return -1;
+	if (listen(ssock_fd, 5) < 0) return -1;
+	return ssock_fd;
+}
+
+int accept_connection(int ssock_fd, struct sockaddr_in clientaddr) {
+	unsigned clientlen = sizeof(clientaddr);
+	int childfd = accept(ssock_fd,
+	(struct sockaddr *) &clientaddr,
+	&clientlen);
+	if (childfd < 0)
+	return -1;
+	return childfd;
+}
+
+int chat_with_client(struct Calc *calc, int client_fd) {
+
+	//printf("ENTERED \n");
+	rio_t rio; int val;
+	char* temp = "";
+	rio_readinitb(&rio, client_fd);
+	char buf[1024];
+	// Read line from client
+	ssize_t rc = rio_readlineb(&rio, buf, sizeof(buf)-1);
+	if (rc < 0) { return 1; } // error reading data from client
+	buf[rc] = '\0';
+
+	printf("THIS IS STRING: %s\n", buf);
+
+	if (strcmp(buf, "quit\n") == 0 || strcmp(buf, "quit\r\n") == 0) {
+		return 0;
+	} else {
+		FILE *in = fmemopen(buf, (size_t) rc, "r");
+		while (fscanf(in, "%s", temp) == 1) {
+			strcat(buf, temp);
+		}
+	  int error = calc_eval(calc, buf, &val);
+    if(error == 0) {
+      return 0;
+    }
+    //this is printing the buf.
+    //Why?
+		rio_writen(client_fd, buf, strlen(buf));
+		return 1;
+	}
+}
+	
+int main(int argc, char **argv) {
+	/* TODO: implement this program */
+	//I literally just copies and pasted this form Lecture 27
+	int port = atoi(argv[1]);
+	//default to 1024
+	if (port < 1024) {
+		port = 1024;
+	}
+
+	//create the calculator
+	struct Calc *calc = calc_create();
+	//not given the port number
+	if (argc != 2) { 
+		return -1; 
+		}
+	int server_fd = open_listenfd(argv[1]);
+	if (server_fd < 0) { return -1; }
+	int keep_going = 1;
+	while (keep_going) {
+		int client_fd = Accept(server_fd, NULL, NULL);
+		if (client_fd > 0) {
+			keep_going = chat_with_client(calc, client_fd);
+			close(client_fd); // close the connection
+		}
+	}
+	close(server_fd); // close server socket
+	return 0;
+}
